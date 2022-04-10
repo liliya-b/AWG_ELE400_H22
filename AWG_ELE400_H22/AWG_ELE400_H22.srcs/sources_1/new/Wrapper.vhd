dda26b5d5080d67291d546b9b9b864d17bae0377
AQ45350 <thomas.giguere-sturrock.1@ens.etsmtl.ca>
2022-04-09 23:13:20 -0400

    Wrapper.vhd rajouté

--- AWG_ELE400_H22/AWG_ELE400_H22.srcs/sources_1/new/Wrapper.vhd
+++ AWG_ELE400_H22/AWG_ELE400_H22.srcs/sources_1/new/Wrapper.vhd
@@ -32,12 +32,25 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Wrapper is
---  Port ( );
+PORT(sclk : in STD_LOGIC; -- Signal de clock venant du Arduino
+     mosi : in STD_LOGIC; -- Master Out Slave In
+     ss_n : in STD_LOGIC; -- Slave select (quand low, le master parle au slave (arduino parle au fpga))
+     miso : out STD_LOGIC; -- Master In Slave Out
+     dataout : out STD_LOGIC_VECTOR (15 downto 0));--Waveform en sortie
 end Wrapper;
 
 architecture Behavioral of Wrapper is
-
+            --Signaux de communication entre les modules
+           SIGNAL busy_sig,rx_req_sig,reset_n_sig,tx_load_en_sig : STD_LOGIC; --SIGNAUX entre les 2 modules 
+           SIGNAL sclk_sig,mosi_sig,ss_n_sig,miso_sig : STD_LOGIC; -- Signaux entre les pin de sortie et le module SPI
+           SIGNAL rx_data_sig, tx_load_data_sig : STD_LOGIC_VECTOR(7 DOWNTO 0);
+           SIGNAL dataout : STD_LOGIC_VECTOR(15 downto 0);
+           
 begin
-
+    SPI :        entity work.spi_slave(comm)
+                 PORT MAP(sclk=>sclk_sig,mosi=>mosi_sig ); -- Branchements entre le module SPI et le generateur
+    
+    Generateur : entity work.Generator(Wave_generator)
+                 PORT MAP(busy=busy_sig);
 
 end Behavioral;
